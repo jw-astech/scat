af2804fa785e5388b2e3219b99e3c1d36e6b3ea1
Jason White <jason.white@astechconsulting.com>
2017-06-12 06:34:00 -0400

    instructions update

---
+++ create-tables.sql
@@ -0,0 +1,265 @@
+﻿
+
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[__MigrationHistory] Script Date: 6/12/2017 6:19:50 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[__MigrationHistory] (
+    [MigrationId]    NVARCHAR (150)  NOT NULL,
+    [ContextKey]     NVARCHAR (300)  NOT NULL,
+    [Model]          VARBINARY (MAX) NOT NULL,
+    [ProductVersion] NVARCHAR (32)   NOT NULL
+);
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[AspNetRoles] Script Date: 6/12/2017 6:20:39 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetRoles] (
+    [Id]   NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex]
+    ON [dbo].[AspNetRoles]([Name] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetRoles]
+    ADD CONSTRAINT [PK_dbo.AspNetRoles] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[AspNetUserClaims] Script Date: 6/12/2017 6:21:07 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUserClaims] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]     NVARCHAR (128) NOT NULL,
+    [ClaimType]  NVARCHAR (MAX) NULL,
+    [ClaimValue] NVARCHAR (MAX) NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [dbo].[AspNetUserClaims]([UserId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserClaims]
+    ADD CONSTRAINT [PK_dbo.AspNetUserClaims] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserClaims]
+    ADD CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE;
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[AspNetUserLogins] Script Date: 6/12/2017 6:21:51 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUserLogins] (
+    [LoginProvider] NVARCHAR (128) NOT NULL,
+    [ProviderKey]   NVARCHAR (128) NOT NULL,
+    [UserId]        NVARCHAR (128) NOT NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [dbo].[AspNetUserLogins]([UserId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserLogins]
+    ADD CONSTRAINT [PK_dbo.AspNetUserLogins] PRIMARY KEY CLUSTERED ([LoginProvider] ASC, [ProviderKey] ASC, [UserId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserLogins]
+    ADD CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE;
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[AspNetUserRoles] Script Date: 6/12/2017 6:22:06 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUserRoles] (
+    [UserId] NVARCHAR (128) NOT NULL,
+    [RoleId] NVARCHAR (128) NOT NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [dbo].[AspNetUserRoles]([UserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_RoleId]
+    ON [dbo].[AspNetUserRoles]([RoleId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]
+    ADD CONSTRAINT [PK_dbo.AspNetUserRoles] PRIMARY KEY CLUSTERED ([UserId] ASC, [RoleId] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]
+    ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE;
+
+
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]
+    ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE;
+
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[AspNetUsers] Script Date: 6/12/2017 6:22:19 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [Hometown]             NVARCHAR (MAX) NULL,
+    [Email]                NVARCHAR (256) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (256) NOT NULL,
+    [UserBioText]          NVARCHAR (MAX) NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([UserName] ASC);
+
+
+GO
+ALTER TABLE [dbo].[AspNetUsers]
+    ADD CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC);
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[UserComments] Script Date: 6/12/2017 6:22:36 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserComments] (
+    [commentid]   INT            IDENTITY (1, 1) NOT NULL,
+    [messageId]   INT            NOT NULL,
+    [commeterId]  NVARCHAR (128) NOT NULL,
+    [commentText] NVARCHAR (MAX) NOT NULL
+);
+
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[UserMessages] Script Date: 6/12/2017 6:22:50 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserMessages] (
+    [messageId]   INT            IDENTITY (1, 1) NOT NULL,
+    [posterId]    NVARCHAR (128) NOT NULL,
+    [messageText] NVARCHAR (MAX) NOT NULL,
+    [postedDate]  DATETIME       NOT NULL
+);
+
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[UserRelationShips] Script Date: 6/12/2017 6:23:02 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserRelationShips] (
+    [connectionId] INT            IDENTITY (1, 1) NOT NULL,
+    [followerId]   NVARCHAR (128) NOT NULL,
+    [followingId]  NVARCHAR (128) NOT NULL
+);
+
+--
+USE [scat]
+GO
+
+/****** Object: Table [dbo].[UserViews] Script Date: 6/12/2017 6:23:16 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserViews] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [messageId]  INT            NOT NULL,
+    [viewedById] NVARCHAR (128) NOT NULL,
+    [liked]      BIT            NOT NULL
+);
+
+
